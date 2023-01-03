57cfd11d1c0fac90d25a3a5d5dac479868557fb1
Robert Ryan <33040161+rtryan98@users.noreply.github.com>
2023-01-03 13:13:33 +0100

    Added custom stack-based function

---
+++ src/LWGE/Common/Function.hpp
@@ -0,0 +1,44 @@
+#pragma once
+
+#include <cstdlib>
+#include <concepts>
+
+namespace lwge
+{
+	template<class Functor>
+	concept LambdaConcept = requires(Functor&& f)
+	{
+		&Functor::operator();
+		{
+			Functor{ std::move(f) }
+		} -> std::same_as<Functor>;
+	};
+
+	template<std::size_t StackSize>
+	class Function
+	{
+		static_assert(StackSize >= 8);
+	private:
+		struct Impl {};
+		using PfnMember = void(Impl::*)() const;
+
+	public:
+		template<LambdaConcept L>
+		Function(L&& lambda)
+		{
+			static_assert(sizeof(L <= StackSize),
+				"Overflow behaviour not yet implemented.");
+			new (m_buf) L{ std::move(lambda) };
+			m_func = reinterpret_cast<PfnMember>(&L::operator());
+		}
+
+		void operator()()
+		{
+			(reinterpret_cast<Impl*>(m_buf)->*m_func)();
+		}
+
+	private:
+		PfnMember m_func;
+		std::byte m_buf[StackSize];
+	};
+}
