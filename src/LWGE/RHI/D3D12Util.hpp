60dbd8ae2becfd759d9ee9d22f86ed285e56a743
Robert <33040161+rtryan98@users.noreply.github.com>
2023-01-08 22:56:50 +0100

    Moved using declaration of ComPtr into lwge::rhi namespace to reduce namespace pollution

--- src/LWGE/RHI/D3D12Util.hpp
+++ src/LWGE/RHI/D3D12Util.hpp
@@ -4,10 +4,10 @@
 #include <cstdint>
 #include <wrl.h>
 
-using Microsoft::WRL::ComPtr;
-
 namespace lwge::rhi
 {
+	using Microsoft::WRL::ComPtr;
+
 	constexpr static uint32_t MAX_CONCURRENT_GPU_FRAMES = 2;
 
 	void throw_if_failed(HRESULT hr);
